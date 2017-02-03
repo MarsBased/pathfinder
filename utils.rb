01f4b45d95fdda386174a054cc713e3d80e7f54c
Francisco Lopez <fjsanpedro@gmail.com>
2017-02-03 13:24:05 +0100

    Testing

--- utils.rb
+++ utils.rb
@@ -1,4 +1,12 @@
-def ask_with_default(question, default:)
-  answer = ask("#{question} (Default #{default})")
-  answer.empty? ? default : answer
+class Utils
+
+  def initialize(template)
+    @template = template
+  end
+
+  def ask_with_default(question, default:)
+    answer = @template.ask("#{question} (Default #{default})")
+    answer.empty? ? default : answer
+  end
+
 end
