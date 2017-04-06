a7c927cb79e80c6cbb3c44b8117ee0e9eef1b119
Xavier <xavier.redo@marsbased.com>
2017-04-06 11:42:18 +0200

    Add marsman/boostrap/default styles for simple form

--- recipes/simple_form.rb
+++ recipes/simple_form.rb
@@ -2,12 +2,44 @@ module Recipes
   class SimpleForm < Base
 
     def gems
-      # Forms
       @template.gem 'simple_form'
     end
 
     def init_file
-      @template.generate 'simple_form:install'
+      run_generators
+      add_sample_i18n
+    end
+
+    private
+
+    def run_generators
+      case ask_framework_for_forms
+      when 'marsman'
+        @template.initializer 'simple_form.rb', File.read(simple_form_marsman_template)
+      when 'bootstrap'
+        @template.generate 'simple_form:install --bootstrap'
+      else
+        @template.generate 'simple_form:install'
+      end
+    end
+
+    def add_sample_i18n
+      @template.run 'rm config/locales/simple_form.en.yml'
+      @template.append_to_file 'config/locales/en.yml',
+                               File.read(simple_form_locale_sample)
+    end
+
+    def ask_framework_for_forms
+      @template.ask('What framework do you want to use for your forms?',
+                    limited_to: %w(marsman bootstrap default))
+    end
+
+    def simple_form_marsman_template
+      File.join(File.dirname(__FILE__), 'simple_form', 'marsman.rb')
+    end
+
+    def simple_form_locale_sample
+      File.join(File.dirname(__FILE__), 'simple_form', 'en.yml')
     end
   end
 end
