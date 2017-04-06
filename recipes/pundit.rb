ac31ee3d4d2cc572977518a7ae2b468b5690583e
Francisco Lopez <fjsanpedro@gmail.com>
2017-04-06 11:33:54 +0200

    Fixed pundit include indentation

--- recipes/pundit.rb
+++ recipes/pundit.rb
@@ -7,9 +7,9 @@ module Recipes
 
     def init_file
       @template.generate 'pundit:install'
-      @template.insert_into_file 'app/controllers/application_controller.rb', after: "class ApplicationController < ActionController::Base\n" do <<~RUBY
-        include Pundit
-        RUBY
+      @template.insert_into_file 'app/controllers/application_controller.rb', after: "class ApplicationController < ActionController::Base\n" do <<-RUBY
+  include Pundit
+      RUBY
       end
     end
   end
