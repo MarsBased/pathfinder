40fed806265b1ce912506e1b92771e0a6e09bd4f
David Gómez <david.gomez@marsbased.com>
2018-10-18 15:32:40 +0200

    Add Sentry recipe and configuration

---
+++ recipes/sentry.rb
@@ -0,0 +1,14 @@
+module Recipes
+  class Sentry < Base
+
+    def gems
+      @template.gem 'sentry-raven'
+    end
+
+    def cook
+      @template.inside 'config' do
+        @template.append_file 'application.yml.example', "\nSENTRY_DSN: ''"
+      end
+    end
+  end
+end
